47746816fa5dbda13adaf5b369fa3402d033bacc
DrShams <sithis02@mail.ru>
2023-10-05 00:28:16 +0300

    added regression_line.py

--- regression_line.py
+++ regression_line.py
@@ -5,58 +5,45 @@ from scipy.stats import linregress
 from scipy.interpolate import interp1d
 import argparse
 
-class AngularVelocityAnalyzer:
+class PositionAnalyzer:
     def __init__(self, file_path):
         self.file_path = file_path
         self.df = pd.read_excel(file_path)
         self.time_seconds = self.df['Time (ms)'] / 1000  # Convert milliseconds to seconds
 
-    def calculate_velocity(self, body_part='LeftShoulder'):
-        angular_displacement = np.diff(self.df[body_part])
-        angular_displacement = np.insert(angular_displacement, 0, 0)
-        velocity_rad_per_s = angular_displacement / self.time_seconds
-
-        return self.time_seconds, velocity_rad_per_s
-
-    def plot_smoothed_velocity_vs_time(self, body_part='LeftShoulder'):
-        time_seconds, velocity_rad_per_s = self.calculate_velocity(body_part)
+    def plot_smoothed_position_vs_time(self, position_label='X'):
+        position_data = self.df[position_label].values
 
         # Create a cubic spline interpolation
-        spline = interp1d(time_seconds, velocity_rad_per_s, kind='cubic')
+        spline = interp1d(self.time_seconds, position_data, kind='cubic')
 
         # Generate a finer time grid for a smoother curve
-        finer_time_seconds = np.linspace(time_seconds.min(), time_seconds.max(), num=1000)
-        smoothed_velocity_rad_per_s = spline(finer_time_seconds)
+        finer_time_seconds = np.linspace(self.time_seconds.min(), self.time_seconds.max(), num=1000)
+        smoothed_position_data = spline(finer_time_seconds)
 
-        slope, intercept, r_value, p_value, std_err = linregress(time_seconds, velocity_rad_per_s)
-        regression_line = slope * time_seconds + intercept
+        slope, intercept, r_value, p_value, std_err = linregress(self.time_seconds, position_data)
+        regression_line = slope * self.time_seconds + intercept
 
         # Create a plot
-        plt.plot(time_seconds, velocity_rad_per_s, 'o', label='Data Points', markersize=4, alpha=0.5)
-        plt.plot(finer_time_seconds, smoothed_velocity_rad_per_s, '-', label='Smoothed Curve', color='blue', linewidth=2)
-        plt.plot(time_seconds, regression_line, color='red', label='Regression Line', linestyle='dashed')
+        plt.plot(self.time_seconds, position_data, 'o', label='Data Points', markersize=4, alpha=0.5)
+        plt.plot(finer_time_seconds, smoothed_position_data, '-', label='Smoothed Curve', color='blue', linewidth=2)
+        plt.plot(self.time_seconds, regression_line, color='red', label='Regression Line', linestyle='dashed')
         plt.xlabel('Time (s)')
-        plt.ylabel('Velocity (rad/s)')
-        plt.title(f'Regression Fit: {body_part} Velocity vs. Time with Smoothed Curve')
+        plt.ylabel(f'Position ({position_label})')
+        plt.title(f'Regression Fit: {position_label} Position vs. Time with Smoothed Curve')
         plt.legend()
         plt.grid(True)
 
         # Show the plot
         plt.show()
 
-        # Calculate the velocity in m/s (convert from rad/s to m/s)
-        velocity_m_per_s = velocity_rad_per_s * 0.149  # Replace with the appropriate conversion factor
-
-        # Display the calculated velocity
-        print(f"Calculated velocity: {velocity_m_per_s.mean():.2f} m/s")
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Plot Angular Velocity and Regression Line')
+    parser = argparse.ArgumentParser(description='Plot Position and Regression Line')
     parser.add_argument('file_path', type=str, help='Path to the Excel file')
-    parser.add_argument('body_part', type=str, help='Body part (e.g., LeftShoulder, RightShoulder)')
+    parser.add_argument('position_label', type=str, help='Position label (e.g., X, Y, Z)')
     args = parser.parse_args()
 
     file_path = args.file_path
-    body_part = args.body_part
-    analyzer = AngularVelocityAnalyzer(file_path)
-    analyzer.plot_smoothed_velocity_vs_time(body_part)
+    position_label = args.position_label
+    analyzer = PositionAnalyzer(file_path)
+    analyzer.plot_smoothed_position_vs_time(position_label)
